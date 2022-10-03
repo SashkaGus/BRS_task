ba625adccc94e28279bd1a9453816fc4e523108b
SashkaGus <a.a.gusev9866@gmail.com>
2022-10-03 16:39:19 +0300

    Last fix

--- Final_stepik_QA_proj/pages/base_page.py
+++ Final_stepik_QA_proj/pages/base_page.py
@@ -6,7 +6,6 @@ from selenium.webdriver.support import expected_conditions as EC
 
 from .locators import BasePageLocators
 import math
-import time
 
 class BasePage():
     def __init__(self, browser, url, timeout=10) -> None:
